55e7819a2efe103d5bcfd50b2f5433716de25c86
Silvio <silvio@silvio-AO722.(none)>
2013-05-08 15:56:50 +0200

    register.html und login.html eingefügt und in der urls.py angegeben

--- ProsDataBase/ProsDataBase/urls.py
+++ ProsDataBase/ProsDataBase/urls.py
@@ -18,6 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     (r'^table/$', TemplateView.as_view(template_name="table_overview.html")),
+    (r'^register/$', TemplateView.as_view(template_name="register.html")),
+    (r'^login/$', TemplateView.as_view(template_name="login.html")),
+
 
 )
 
