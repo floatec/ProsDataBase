fcbd397fc3d10f62233d4542b6d6aa266637493d
floatec <mp@floatec.de>
2013-05-20 17:25:07 +0200

    first step input mask

--- ProsDataBase/database/views/frontend.py
+++ ProsDataBase/database/views/frontend.py
@@ -1,9 +1,10 @@
-from ProsDataBase.database.models import Column *
-def insertDataset(request, table_id, template='ProsDataBase/templates/dataset.html'):
+from ..models import *
+from django.shortcuts import render
+def insertDataset(request, table_id=1, template='dataset.html'):
     """Render insert mask for dataset"""
-    structure=Column.objects.filter(tabele.id=table_id)
+    structure=Column.objects.filter(table=table_id)
 
 
     return render(request, template, {
-        'structure': strcuture,
+        'strcuture': structure
     })
