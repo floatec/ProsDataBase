8f216ebc660fa777e5308008c143f14f4abff285
thor <huse-yin@hotmail.de>
2013-05-23 14:23:25 +0200

    New changelist

--- ProsDataBase/database/factory.py
+++ ProsDataBase/database/factory.py
@@ -2,7 +2,10 @@ from database.models import *
 from database.forms import *
 
 def create_table(**kwargs):
-    """ creates a table"""
+    """
+    creates a simple table with two columns with
+    two datasets filed with two values
+    """
 
     table = dict()
     table["name"] = "Test"
@@ -10,15 +13,94 @@ def create_table(**kwargs):
     tabF = TableForm(table)
     if tabF.is_valid():
         newTable = tabF.save()
+        table.save()
 
-    column = dict()
-    column["name"] = "A"
-    column["required"] = True
-    column["created"] = datetime.now()
-    colF = ColumnForm(column)
-    if colF.is_valid():
-        newColumn = colF.save()
+    columnFirstname= dict()
+    columnFirstname["name"] = "Vorname"
+    columnFirstname["required"] = True
+    columnFirstname["created"] = datetime.now()
+    colFirstnameF = ColumnForm(columnFirstname)
+    if colFirstnameF.is_valid():
+        newColumnFirstname = colFirstnameF.save()
+        newColumnFirstname.table = newTable
+        newColumnFirstname.save()
 
-    newColumn.table = newTable
+    columnSecondname = dict()
+    columnSecondname["name"] = "Nachname"
+    columnSecondname["required"] = True
+    columnSecondname["created"] = datetime.now()
+    colSecondnameF = ColumnForm(columnSecondname)
+    if colSecondnameF.is_valid():
+        newColumnSecondname = colSecondnameF.save()
+        newColumnSecondname.table = newTable
+        newColumnSecondname.save()
 
-    data = Data.objects.create()
+    dataSetFirstname = dict()
+    dataSetFirstname["created"] = datetime.now()
+    dataSetFirstnameF = DatasetForm(dataSetFirstname)
+    if dataSetFirstnameF.is_valid():
+        newDataSetFirstname = dataSetFirstnameF.save()
+        newDataSetFirstname.column = newColumnFirstname
+        newDataSetFirstname.save()
+
+    dataSetSecondname = dict()
+    dataSetSecondname["created"] = datetime.now()
+    dataSetSecondnameF = DatasetForm(dataSetSecondname)
+    if dataSetSecondnameF.is_valid():
+        newDataSetSecondname = dataSetSecondnameF.save()
+        newDataSetSecondname.column = newColumnSecondname
+        newDataSetSecondname.save()
+
+    dataTextFirstname = dict()
+    dataTextFirstname["created"] = datetime.now()
+    dataTextFirstname["content"] = "Hasret"
+    dataTextFirstnameF = DataTextForm(dataTextFirstname)
+    if dataTextFirstnameF.is_valid():
+        newDataTextFirstname = dataTextFirstnameF.save()
+        newDataTextFirstname.dataSet = newDataSetFirstname
+        newDataTextFirstname.save()
+
+    dataTextSecondname = dict()
+    dataTextSecondname["created"] = datetime.now()
+    dataTextSecondname["content"] = "Demirci"
+    dataTextSecondnameF = DataTextForm(dataTextSecondname)
+    if dataTextSecondnameF.is_valid():
+        newDataTextSecondname = dataTextSecondnameF.save()
+        newDataTextSecondname.dataSet = newDataSetSecondname
+        newDataTextSecondname.save()
+
+def create_Group(**kwargs):
+    """
+    creates a group with two members
+    """
+    hans = DBUser.objects.create_user(username="hans")
+    hans.save()
+    mark = DBUser.objects.create_user(username="mark")
+    mark.save()
+
+    groupA = dict()
+    groupA["name"] = "Student"
+    groupF = DBGroupForm(groupA)
+    if groupF.is_valid():
+        newDBGroup = groupF.save()
+        newDBGroup.save()
+
+    m1 = dict()
+    m1["isAdmin"] = True
+    m1F = MembershipForm(m1)
+    if m1F.is_valid():
+        m1new = m1F.save(commit=False)
+        m1new.user = hans
+        m1new.group = newDBGroup
+        m1new.save()
+
+    m2 = dict()
+    m2["isAdmin"] = False
+    m2F = MembershipForm(m2)
+    if m2F.is_valid():
+        m2new = m2F.save(commit=False)
+        m2new.user = mark
+        m2new.group = newDBGroup
+        m2F.save()
+
+    return newDBGroup
